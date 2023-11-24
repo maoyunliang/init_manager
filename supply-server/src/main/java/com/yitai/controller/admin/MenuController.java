1231a75679d87d4c9e3d799aa07e335d713fbb40
18879182511 <18879182511@163.com>
2023-11-24 16:10:31 +0800

    菜单后台管理完善2.1.2(java ExcelUtil报表导入导出)

--- supply-server/src/main/java/com/yitai/controller/admin/MenuController.java
+++ supply-server/src/main/java/com/yitai/controller/admin/MenuController.java
@@ -17,8 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -52,10 +50,7 @@ public class MenuController {
     @GetMapping("/export")
 //    @HasPermit(permission = "sys:menu:export")
     public void export(HttpServletResponse httpResponse){
-        List<Object> head = Arrays.asList("菜单名称", "组件路径", "菜单路由" );
         List<MenuVO> menuVOList = menuService.list();
-        List<List<Object>> data = new ArrayList<>();
-        data.add(head);
         ExcelUtils.export(httpResponse,"用户表", menuVOList, MenuVO.class);
 //        return Result.success();
     }
