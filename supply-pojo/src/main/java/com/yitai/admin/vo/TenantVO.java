5160d94add94a6566d31d6cff7278f448fdf09dc
毛云亮 <18879182511@163.com>
2023-12-12 21:09:52 +0800

    获取企业信息

--- supply-pojo/src/main/java/com/yitai/admin/vo/TenantVO.java
+++ supply-pojo/src/main/java/com/yitai/admin/vo/TenantVO.java
@@ -21,10 +21,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Schema(description = "租户信息返回数据格式")
 public class TenantVO {
-    @Schema(name = "租户id")
+    @Schema(description = "租户id")
     private Long id;
-    @Schema(name = "租户名")
+    @Schema(description = "租户名")
     private String tenantName;
-    @Schema(name = "租户logo")
+    @Schema(description = "租户logo")
     private String tenantLogo;
+    @Schema(description = "联系方式")
+    private String contact;
+    @Schema(description = "地区")
+    private String location;
+    @Schema(description = "详细地址")
+    private String address;
+    @Schema(description = "相关资质")
+    private String qualification;
+    @Schema(description = "备注")
+    private String remark;
+    @Schema(description = "状态")
+    private Integer status;
 }
